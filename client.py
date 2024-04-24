3d364005fa3a129e7fc5e3e8ce0f923dd7ef488c
csb30 <30balint@gmail.com>
2024-04-24 12:35:58 +0200

    server gui: ip input, start button, logging, progress bar, results client gui: ip input, start button, logging

--- client.py
+++ client.py
@@ -9,16 +9,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-from datasets.utils.logging import disable_progress_bar
+# from datasets.utils.logging import disable_progress_bar
 from torch.utils.data import DataLoader
+import time
 
 import flwr as fl
 from flwr.common import Metrics
-from flwr_datasets import FederatedDataset
 
 from typing import Dict, Optional
 
+import customtkinter as ctk
+import threading
+
 DEVICE = torch.device("cpu")
+
+LOGFILE="client"+sys.argv[1]+".txt"
+
+f = open(LOGFILE, "wt")
+f.close()
+fl.common.logger.configure(identifier="client", filename=LOGFILE)
 
 class Net(nn.Module):
     def __init__(self) -> None:
@@ -103,7 +112,7 @@ class FlowerClient(fl.client.NumPyClient):
     def fit(self, parameters, config):
         # Read values from config
         server_round = config["server_round"]
-        #local_epochs = config["local_epochs"]
+        # local_epochs = config["local_epochs"]
 
         # Use values provided by the config
         print(f"[Client {self.cid}] fit, round {server_round}, config: {config}")
@@ -118,47 +127,128 @@ class FlowerClient(fl.client.NumPyClient):
         return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
 
 
-def load_datasets(NUM_CLIENTS=10, BATCH_SIZE=32):
-    fds = FederatedDataset(dataset="cifar10", partitioners={"train": NUM_CLIENTS})
 
-    def apply_transforms(batch):
-        # Instead of passing transforms to CIFAR10(..., transform=transform)
-        # we will use this function to dataset.with_transform(apply_transforms)
-        # The transforms object is exactly the same
-        transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ]
-        )
-        batch["img"] = [transform(img) for img in batch["img"]]
-        return batch
-
-    # Create train/val for each partition and wrap it into DataLoader
-    trainloaders = []
-    valloaders = []
-    for partition_id in range(NUM_CLIENTS):
-        partition = fds.load_partition(partition_id, "train")
-        partition = partition.with_transform(apply_transforms)
-        partition = partition.train_test_split(train_size=0.8)
-        trainloaders.append(DataLoader(partition["train"], batch_size=BATCH_SIZE))
-        valloaders.append(DataLoader(partition["test"], batch_size=BATCH_SIZE))
-    testset = fds.load_split("test").with_transform(apply_transforms)
-    testloader = DataLoader(testset, batch_size=BATCH_SIZE)
-    return trainloaders, valloaders, testloader
-
-trainloaders, valloaders, testloader = load_datasets()
-net = Net()
-
-def client_fn(cid, DEVICE) -> FlowerClient:
-    net = Net().to(DEVICE)
-    trainloader = trainloaders[int(cid)]
-    valloader = valloaders[int(cid)]
-    return FlowerClient(cid, net, trainloader, valloader)
 
 # Start Flower client
-fl.client.start_client(
-    server_address="localhost:" + str(sys.argv[1]),
-    client=client_fn(sys.argv[2], DEVICE).to_client(),
-    grpc_max_message_length=1024 * 1024 * 1024
-)
+
+class FlowerClientGUI:
+    def __init__(self, clientnum: int):
+        self.trainloaders = None
+        self.valloaders = None
+        self.testloader = None
+        self.clientnum = clientnum
+
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
+
+        self.root = ctk.CTk()
+        self.root.geometry("500x350")
+        self.root.title("Flower Client " + str(clientnum))
+
+        self.frame = ctk.CTkFrame(self.root)
+
+        title = "Flower Client " + str(clientnum) + " GUI"
+        self.label = ctk.CTkLabel(self.frame, text=title, font=('Roboto', 24))
+
+        self.status = ctk.CTkLabel(self.frame, text="Press Start", font=('Roboto', 15))
+        self.progress = ctk.CTkProgressBar(self.frame)
+        self.progress.set(0)
+
+        self.scroll = ctk.CTkScrollableFrame(self.frame)
+        self.log = ctk.CTkLabel(self.scroll, text="Press Start", font=('Roboto', 10), justify="left")
+
+        self.th_client = threading.Thread(target=self.start_client)
+        self.th_log = threading.Thread(target=self.update_log)
+
+        self.ipinput = ctk.CTkTextbox(self.frame, height=10)
+        self.ipinput.insert(ctk.END, "localhost:8500")
+        self.button = ctk.CTkButton(self.frame, text="Loading datasets...",
+                                    command=lambda: [self.th_client.start(), self.th_log.start()], state="disabled")
+
+        th_load = threading.Thread(target=self.load_datasets)
+        th_load.start()
+
+    def start_client(self):
+        #self.status.pack(padx=10, pady=12)
+        #self.progress.pack(padx=10, pady=12)
+        self.scroll.pack(padx=20, pady=20, fill="both", expand=True)
+        self.log.pack(padx=10, pady=12, anchor="w")
+
+        self.button.pack_forget()
+        ip = self.ipinput.get("0.0", ctk.END)
+        self.ipinput.pack_forget()
+
+        fl.client.start_client(
+            server_address=ip,
+            client=self.client_fn(self.clientnum, DEVICE).to_client(),
+            grpc_max_message_length=1024 * 1024 * 1024
+        )
+
+    def start_gui(self):
+        self.frame.pack(padx=20, pady=20, fill="both", expand=True)
+
+        self.label.pack(padx=10, pady=12)
+
+        self.ipinput.pack(padx=10, pady=12)
+        self.button.pack(padx=10, pady=12)
+
+        self.root.mainloop()
+
+    def load_datasets(self, NUM_CLIENTS=10, BATCH_SIZE=32):
+        from flwr_datasets import FederatedDataset
+
+        fds = FederatedDataset(dataset="cifar10", partitioners={"train": NUM_CLIENTS})
+
+        def apply_transforms(batch):
+            # Instead of passing transforms to CIFAR10(..., transform=transform)
+            # we will use this function to dataset.with_transform(apply_transforms)
+            # The transforms object is exactly the same
+            transform = transforms.Compose(
+                [
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                ]
+            )
+            batch["img"] = [transform(img) for img in batch["img"]]
+            return batch
+
+        # Create train/val for each partition and wrap it into DataLoader
+        self.trainloaders = []
+        self.valloaders = []
+        for partition_id in range(NUM_CLIENTS):
+            partition = fds.load_partition(partition_id, "train")
+            partition = partition.with_transform(apply_transforms)
+            partition = partition.train_test_split(train_size=0.8)
+            self.trainloaders.append(DataLoader(partition["train"], batch_size=BATCH_SIZE))
+            self.valloaders.append(DataLoader(partition["test"], batch_size=BATCH_SIZE))
+        testset = fds.load_split("test").with_transform(apply_transforms)
+        self.testloader = DataLoader(testset, batch_size=BATCH_SIZE)
+
+        self.button.configure(state="normal", text="Start client")
+
+    def client_fn(self, cid, DEVICE) -> FlowerClient:
+        net = Net().to(DEVICE)
+        trainloader = self.trainloaders[int(cid)]
+        valloader = self.valloaders[int(cid)]
+        return FlowerClient(cid, net, trainloader, valloader)
+
+    def update_log(self):
+        f = open(LOGFILE, "rt")
+        text = ""
+        while self.th_client.is_alive():
+            line = f.readline()
+            while line != "":
+                text += line.split("|")[3]
+                self.log.configure(text=text)
+                line = f.readline()
+            time.sleep(1)
+        line = f.readline()
+        while line != "":
+            text += line.split("|")[3]
+            self.log.configure(text=text)
+            line = f.readline()
+        f.close()
+
+
+client = FlowerClientGUI(int(sys.argv[1]))
+client.start_gui()
--- server.py
+++ server.py
@@ -1,9 +1,47 @@
 import flwr as fl
 import sys
+
+import flwr.server
 import numpy as np
+import customtkinter as ctk
+import threading
+import time
+
+# https://github.com/PratikGarai/MNIST-Federated/tree/master/03_Non%20IID%20Demo
+from flwr.common import Parameters, Scalar
+
+f = open("server.txt", "wt")
+f.close()
+fl.common.logger.configure(identifier="server", filename="server.txt")
 
 
-#https://github.com/PratikGarai/MNIST-Federated/tree/master/03_Non%20IID%20Demo
+class SaveModelStrategy(fl.server.strategy.FedAvg):
+    def __init__(self, label: ctk.CTkLabel, progress: ctk.CTkProgressBar, rounds=3):
+        super().__init__(on_fit_config_fn=fit_config)
+        self.label = label
+        self.rounds = rounds
+        self.progress = progress
+
+    def aggregate_fit(
+            self,
+            rnd,
+            results,
+            failures
+    ):
+        aggregated_weights = super().aggregate_fit(rnd, results, failures)
+        if aggregated_weights is not None:
+            # Save aggregated_weights
+            print(f"Saving round {rnd} aggregated_weights...")
+            np.savez(f"round-{rnd}-weights.npz", *aggregated_weights)
+            self.update_status(rnd)
+        return aggregated_weights
+
+    def update_status(self, rnd):
+        text = "Round " + str(rnd) + " / " + str(self.rounds)
+        self.label.configure(text=text)
+        x = rnd / self.rounds
+        self.progress.set(x)
+
 
 def fit_config(server_round: int):
     """Return training configuration dict for each round.
@@ -17,31 +55,93 @@ def fit_config(server_round: int):
     return config
 
 
-class SaveModelStrategy(fl.server.strategy.FedAvg):
+class FlowerServer:
     def __init__(self):
-        super().__init__(on_fit_config_fn=fit_config)
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
 
-    def aggregate_fit(
-            self,
-            rnd,
-            results,
-            failures
-    ):
-        aggregated_weights = super().aggregate_fit(rnd, results, failures)
-        if aggregated_weights is not None:
-            # Save aggregated_weights
-            print(f"Saving round {rnd} aggregated_weights...")
-            np.savez(f"round-{rnd}-weights.npz", *aggregated_weights)
-        return aggregated_weights
+        self.root = ctk.CTk()
+        self.root.geometry("1000x500")
+        self.root.title("Flower Server")
+
+        self.frame = ctk.CTkFrame(self.root)
+
+        self.label = ctk.CTkLabel(self.frame, text="Flower Server GUI", font=('Roboto', 24))
+
+        self.status = ctk.CTkLabel(self.frame, text="Press Start", font=('Roboto', 15))
+        self.progress = ctk.CTkProgressBar(self.frame)
+        self.progress.set(0)
+
+        self.scroll = ctk.CTkScrollableFrame(self.frame)
+        self.log = ctk.CTkLabel(self.scroll, text="Press Start", font=('Roboto', 15), justify="left")
+
+        self.th_server = threading.Thread(target=self.start_server)
+        self.th_log = threading.Thread(target=self.update_log)
+
+        self.ipinput = ctk.CTkTextbox(self.frame, height=10)
+        self.ipinput.insert(ctk.END, "localhost:8500")
+        self.button = ctk.CTkButton(self.frame, text="Start Server",
+                                    command=lambda: [self.th_server.start(), self.th_log.start()])
+
+        self.history = flwr.server.History()
+
+        self.strategy = SaveModelStrategy(self.status, self.progress)
+        self.total_rounds = 3
+
+    def finish(self):
+        text = ""
+        for i in self.history.losses_distributed:
+            round, loss = i
+            text += "round " + str(round) + ": " + str(loss * 100) + "%\n"
+        self.status.configure(text=text)
+
+    def start_server(self, rounds=3):
+        self.status.pack(padx=10, pady=12)
+        self.progress.pack(padx=10, pady=12)
+        self.scroll.pack(padx=20, pady=20, fill="both", expand=True)
+        self.log.pack(padx=10, pady=12, anchor="w")
+
+        self.button.pack_forget()
+        ip = self.ipinput.get("0.0", ctk.END)
+        self.ipinput.pack_forget()
+
+        self.status.configure(text="Waiting for results...")
+        self.total_rounds = rounds
+        self.history = fl.server.start_server(
+            server_address=ip,
+            config=fl.server.ServerConfig(num_rounds=rounds),
+            grpc_max_message_length=1024 * 1024 * 1024,
+            strategy=self.strategy,
+        )
+        self.finish()
+
+    def update_log(self):
+        f = open("server.txt", "rt")
+        text = ""
+        while self.th_server.is_alive():
+            line = f.readline()
+            while line != "":
+                text += line.split("|")[3]
+                self.log.configure(text=text)
+                line = f.readline()
+            time.sleep(1)
+        line = f.readline()
+        while line != "":
+            text += line.split("|")[3]
+            self.log.configure(text=text)
+            line = f.readline()
+        f.close()
+
+    def start_gui(self):
+        self.frame.pack(padx=20, pady=20, fill="both", expand=True)
+
+        self.label.pack(padx=10, pady=12)
+
+        self.ipinput.pack(padx=10, pady=12)
+        self.button.pack(padx=10, pady=12)
+
+        self.root.mainloop()
 
 
-# Create strategy and run server
-strategy = SaveModelStrategy()
-
-# Start Flower server for three rounds of federated learning
-fl.server.start_server(
-    server_address='localhost:' + str(sys.argv[1]),
-    config=fl.server.ServerConfig(num_rounds=3),
-    grpc_max_message_length=1024 * 1024 * 1024,
-    strategy=strategy
-)
+server = FlowerServer()
+server.start_gui()
